cd58bac625c566ccbb00788112fe27ca7a1db0cb
F1shb0ne <f1shb0nes80@gmail.com>
2015-08-27 18:14:35 -0300

    Missed commit for Plugin.yml in adding /langinfo command. Applied further checking against commands that shouldn't be run by console.

--- src/ca/vire/ChatMunger/Plugin.java
+++ src/ca/vire/ChatMunger/Plugin.java
@@ -68,6 +68,11 @@ public final class Plugin extends JavaPlugin {
 
         if (cmd.getName().equalsIgnoreCase("lang")) {
 
+            if (sender.getName().contentEquals("CONSOLE")) {
+                this.getLogger().info("Only players may use this!");
+                return true;
+            }
+
             if (args.length == 1) {
                 Commands.Lang(this, PlayerMgr, LanguageTree, sender, args[0]);
             } else {
@@ -78,6 +83,11 @@ public final class Plugin extends JavaPlugin {
         }
 
         if (cmd.getName().equalsIgnoreCase("teachlang")) {
+            if (sender.getName().contentEquals("CONSOLE")) {
+                this.getLogger().info("Only players may use this!");
+                this.getLogger().info("use /givelang instead");
+                return true;
+            }
             if (args.length == 2) {
                 Commands.TeachLang(this, sender, args[0], args[1]);
             } else {
@@ -87,6 +97,11 @@ public final class Plugin extends JavaPlugin {
         }
 
         if (cmd.getName().equalsIgnoreCase("acceptlang")) {
+            if (sender.getName().contentEquals("CONSOLE")) {
+                this.getLogger().info("Only players may use this!");
+                return true;
+            }
+
             if (args.length == 0) {
                 Commands.AcceptLang(this, sender);
             } else {
@@ -101,6 +116,20 @@ public final class Plugin extends JavaPlugin {
                 Commands.GiveLang(this, PlayerMgr, LanguageTree, sender, args[0], args[1]);
             } else {
                 sender.sendMessage(ChatColor.DARK_RED + "Usage: /givelang <player> <language>");
+            }
+
+            return true;
+        }
+
+        if (cmd.getName().equalsIgnoreCase("langinfo")) {
+            if (args.length == 1) {
+                // Wants information about the language specified in args[0]
+                Commands.LangInfo(this, PlayerMgr, LanguageTree, sender, args[0]);
+                // Wants more general information.
+            } else if (args.length == 0) {
+                Commands.LangInfo(this, PlayerMgr, LanguageTree, sender, null);
+            } else {
+                sender.sendMessage(ChatColor.DARK_RED + "Usage: /langinfo [language]");
             }
 
             return true;
