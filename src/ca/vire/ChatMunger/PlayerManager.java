b1696c0d946bcacfbd1fa9c445ad5f8ce04978fe
F1shb0ne <f1shb0nes80@gmail.com>
2015-08-27 09:20:57 -0300

    development on LoadPlayerData in PlayerManager object: player data in data folder is used, and new SavePlayerData is called on onDisable in Plugin. Added GetPlayerCurrentLanguage, PlayerKnowsLanguage, NumLanguagesKnown, GetLanguageSkillPoints

--- src/ca/vire/ChatMunger/PlayerManager.java
+++ src/ca/vire/ChatMunger/PlayerManager.java
@@ -1,8 +1,17 @@
 package ca.vire.ChatMunger;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
-import org.bukkit.plugin.java.JavaPlugin;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+//This object keeps track of the player data (MungerPlayer objects) for each player
 public class PlayerManager {
 
     // Mapping between player on server and his/her own plugin data
@@ -12,27 +21,124 @@ public class PlayerManager {
     JavaPlugin plugin;
 
     public PlayerManager(JavaPlugin plugin) {
-        PlayerMap = new HashMap<String, MungerPlayer>();        
-        this.plugin = plugin; 
+        PlayerMap = new HashMap<String, MungerPlayer>();
+        this.plugin = plugin;
     }
 
-    // Do we have existing data on the player in memory? 
+    // Do we have existing data on the player in memory?
     private boolean PlayerExists(String player) {
         return PlayerMap.containsKey(player);
     }
 
     // Loads player language information from plugin data folder
+    // We're here because player data doesn't exist in memory, but might on disk
     private boolean LoadPlayerData(String player) {
+        boolean result = false;
+        String PlayerDataUrl = plugin.getDataFolder().getAbsolutePath() + "/Players/" + player + ".yml";
+        String DataURL = plugin.getDataFolder().getAbsolutePath();
+        File PlayerFile = new File(PlayerDataUrl);
+        FileConfiguration PlayerData, LanguageData;
+        Set<String> LangList;
+        int LangCount, LastExchange, CurrentSkillPoints, RequiredSkillPoints;
+
+        // Create new player data object in memory
+        PlayerMap.put(player, new MungerPlayer());
+
+        if (PlayerFile.exists()) {
+            // Player data exists on disk; load it in
+            PlayerData = new YamlConfiguration().loadConfiguration(new File(PlayerDataUrl));
+
+            // Load in data
+            PlayerMap.get(player).CurrentLanguage = PlayerData.getString("CurrentLanguage");
+            PlayerMap.get(player).LastExchange = PlayerData.getInt("LastExchange");
+
+            plugin.getLogger().info(player + "'s last language exchange was " + new Integer(PlayerData.getInt("LastExchange")).toString() + " seconds ago.");
+
+            // Languages player knows
+            LangList = PlayerData.getConfigurationSection("Languages").getKeys(false);
+            LangCount = LangList.size();
+
+            plugin.getLogger().info(player + " knows " + new Integer(LangCount).toString() + " languages.");
+
+            for (String lang: LangList) {
+                CurrentSkillPoints = PlayerData.getInt("Languages." + lang + ".CurrentSkillPoints");
+                RequiredSkillPoints = PlayerData.getInt("Languages." + lang + ".RequiredSkillPoints");
+
+                plugin.getLogger().info(player + " has " + new Integer(CurrentSkillPoints).toString() + " points in " + lang);
+
+                PlayerMap.get(player).LangKnowledge.put(lang, new LanguageProperties(CurrentSkillPoints, RequiredSkillPoints));
+            }
+
+            // Indicate existing data was found to the caller
+            result = true;
+        } else {
+            // Set some safe defaults as needed
+        }
+
+
+        return result;
+    }
+
+    // Update existing / create new player data files
+    public void SavePlayerData() {
+        String url;
+        File PlayerFile;
+        FileConfiguration yml;
+
+        // For each player in memory
+        for (String player: PlayerMap.keySet()) {
+            // Initialize yml object
+            url = plugin.getDataFolder().getAbsolutePath() + "/Players/" + player + ".yml";
+            yml = new YamlConfiguration();
+
+            // Assign data elements
+            yml.set("CurrentLanguage", PlayerMap.get(player).CurrentLanguage);
+
+            // Write the file
+            try {
+                PlayerFile = new File(url);
+                yml.save(PlayerFile);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public String GetPlayerCurrentLanguage(String player) {
+        String language = "Invalid"; // Should never get this
+
+        if (PlayerExists(player)) {
+            // See if there is existing data on the player
+            language = PlayerMap.get(player).CurrentLanguage;
+        } else {
+            LoadPlayerData(player);
+            language = PlayerMap.get(player).CurrentLanguage;
+        }
+
+        return language;
+    }
+
+    public boolean PlayerKnowsLanguage(String player, String language) {
         boolean result = false;
 
         return result;
     }
 
-    public boolean PlayerKnowsLanguage(String player, String language) {
-        boolean result = false;        
+    public int NumLanguagesKnown(String player) {
+        int count = 0;
 
+        if (PlayerExists(player)) {
+            count = PlayerMap.get(player).GetFluentLanguageCount();
+        }
 
-        return result;
+        return count;
+    }
+
+    public int GetLanguageSkillPoints(String player, String language) {
+        int a;
+
+        return PlayerMap.get(player).LangKnowledge.get(language).CurrentSkillPoints;
     }
 
 }
