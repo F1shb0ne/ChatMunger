aec64a353a1126fa4e909e3d50a65bd1aacefde8
F1shb0ne <f1shb0nes80@gmail.com>
2015-08-31 11:25:12 -0300

    language template object LanguageSettings now has support for passive learning

--- src/ca/vire/ChatMunger/LanguageSettings.java
+++ src/ca/vire/ChatMunger/LanguageSettings.java
@@ -5,9 +5,13 @@ public class LanguageSettings {
 
     public int RequiredSkillPoints;
     public long ExchangeCoolDown;
+    public int RequiredExposures;
+    public boolean PassiveLearning;
     
-    LanguageSettings(int requiredpoints, long cooldown) {
+    LanguageSettings(int requiredpoints, long cooldown, int requiredexposures, boolean passivelearning) {
         RequiredSkillPoints = requiredpoints;
         ExchangeCoolDown = cooldown;
+        RequiredExposures = requiredexposures;
+        PassiveLearning = passivelearning;
     }
 }
