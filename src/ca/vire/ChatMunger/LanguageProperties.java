0fac64ae5fb3e743c259efbd1b4a6a5654cd2c6c
F1shb0ne <f1shb0nes80@gmail.com>
2015-08-31 11:22:26 -0300

    Updated LanguageProperties object for passivelearning, added AddExposure method; Exposure points go towards Skill points.

--- src/ca/vire/ChatMunger/LanguageProperties.java
+++ src/ca/vire/ChatMunger/LanguageProperties.java
@@ -5,26 +5,51 @@ package ca.vire.ChatMunger;
  */
 public class LanguageProperties {
 
-    public int CurrentSkillPoints; // Number of skill points the player has gained so far
+    public int CurrentSkillPoints;  // Number of skill points the player has gained so far
     public int RequiredSkillPoints; // Number of skill points required to have learned the language
+    public int CurrentExposures;    // Number of times a player has heard someone /speak in this language
+    public int RequiredExposures;   // Number of exposures required to advance one skill point
+    public boolean PassiveLearning; // Can the language be learned in a passive manner
 
-    public LanguageProperties(int currentSkillPoints, int requiredSkillPoints) {
+    public LanguageProperties(int currentSkillPoints, int requiredSkillPoints, int currentExposures, int requiredExposures, boolean passiveLearning) {
         CurrentSkillPoints = currentSkillPoints;
         RequiredSkillPoints = requiredSkillPoints;
+        CurrentExposures = currentExposures;
+        RequiredExposures = requiredExposures;
+        PassiveLearning = passiveLearning;
     }
-    
+
     // Add or remove a skill point
     public void AddPoint(int point) {
         CurrentSkillPoints += point;
-        
+
         if (CurrentSkillPoints > RequiredSkillPoints)
             CurrentSkillPoints = RequiredSkillPoints;
-        
+
         if (CurrentSkillPoints < 0)
             CurrentSkillPoints = 0;
     }
-    
-    // Return language skill as number between 0 - 100 
+
+    // Return true if exposure resulted in skill point gain
+    public boolean AddExposure(int exposure) {
+        boolean result = false;
+
+        if (PassiveLearning) {
+            CurrentExposures += exposure;
+
+            if (CurrentExposures > RequiredExposures) {
+                AddPoint(1);
+                result = true;
+                CurrentExposures = 0;
+            }
+            if (CurrentExposures < 0)
+                CurrentExposures = 0;
+        }
+
+        return result;
+    }
+
+    // Return language skill as number between 0 - 100
     public int GetSkillPercentage() {
         return (int)(((double)CurrentSkillPoints / (double)RequiredSkillPoints) * 100.0);
     }
